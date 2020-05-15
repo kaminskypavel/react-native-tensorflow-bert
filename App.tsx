f1b802a98728f2f94806e5d7c226a88095216b5f
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-05-15 13:26:11 +0300

    refactor: move App.tsx to class component for using async componentDidMount

--- App.tsx
+++ App.tsx
@@ -1,16 +1,58 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
+import * as qna from '@tensorflow-models/qna'
 
 type Props = {
-    data?: string;
+    sharedText?: string;
 }
-export default function App(props: Props) {
-    return (
-        <View style={styles.container}>
-            <Text>Please Share a Link (Via The Share Menu on Android) To This App</Text>
-            <Text>data : {JSON.stringify(props)}</Text>
-        </View>
-    );
+
+type State = {
+    model: any,
+    isTFReady: boolean,
+    isQnAModelReady: boolean
+
+}
+
+
+export default class App extends React.Component<Props, State> {
+    state = {
+        model: null,
+        isTFReady: false,
+        isQnAModelReady: false
+    }
+
+
+    async componentDidMount() {
+        console.log('loading-tf', new Date());
+
+        await tf.ready()
+        this.setState({isTFReady: true})
+
+        console.log('done loading-tf', new Date());
+        console.log('loading-qna', new Date());
+
+        const model = await qna.load();
+        this.setState({isQnAModelReady: true})
+
+        console.log('done loading-qna', new Date());
+        this.setState({model})
+    }
+
+    render() {
+        const {sharedText} = this.props;
+        const {isQnAModelReady, isTFReady, model} = this.state
+
+        return (
+            <View style={styles.container}>
+                {!sharedText &&
+                <Text>Please Share a Link (Via The Share Menu on Android)</Text>}
+                {!isTFReady && <Text>Loading TensorFlow, Please Wait...</Text>}
+                {isTFReady && !isQnAModelReady && <Text>Loading BERT Model, Please Wait...</Text>}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
